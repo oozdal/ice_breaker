e37c9fc02473fbe61d1b91745fbf2d9b5f4fc033
Ozer, Ozdal <ozerozdal@gmail.com>
2024-03-05 15:32:32 -0500

    adding ice_breaker.py

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,54 +1,33 @@
-from typing import Tuple
-from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
-from agents.twitter_lookup_agent import lookup as twitter_lookup_agent
-from chains.custom_chains import (
-    get_summary_chain,
-    get_interests_chain,
-    get_ice_breaker_chain,
-)
-from third_parties.linkedin import scrape_linkedin_profile
-
-from third_parties.twitter import scrape_user_tweets
-from output_parsers import (
-    summary_parser,
-    topics_of_interest_parser,
-    ice_breaker_parser,
-    Summary,
-    IceBreaker,
-    TopicOfInterest,
-)
+from dotenv import load_dotenv
+from langchain.chains import LLMChain
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
 
 
-def ice_break_with(name: str) -> Tuple[Summary, IceBreaker, TopicOfInterest, str]:
-    linkedin_username = linkedin_lookup_agent(name=name)
-    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username)
+if __name__ == '__main__':
+    load_dotenv()
 
-    twitter_username = twitter_lookup_agent(name=name)
-    tweets = scrape_user_tweets(username=twitter_username)
+    print("Hello LangChain!")
+    information = """
+    Elon Reeve Musk (/ˈiːlɒn/; EE-lon; born June 28, 1971) is a businessman and investor. He is the founder, chairman, CEO, and CTO of SpaceX; angel investor, CEO, product architect, and former chairman of Tesla, Inc.; owner, executive chairman, and CTO of X Corp.; founder of the Boring Company and xAI; co-founder of Neuralink and OpenAI; and president of the Musk Foundation. He is one of the wealthiest people in the world, with an estimated net worth of US$213 billion as of February 2024, according to the Bloomberg Billionaires Index, and $210 billion according to Forbes, primarily from his ownership stakes in Tesla and SpaceX.[5][6]
 
-    summary_chain = get_summary_chain()
-    summary_and_facts = summary_chain.run(
-        information=linkedin_data, twitter_posts=tweets
-    )
-    summary_and_facts = summary_parser.parse(summary_and_facts)
+    A member of the wealthy South African Musk family, Elon was born in Pretoria and briefly attended the University of Pretoria before immigrating to Canada at age 18, acquiring citizenship through his Canadian-born mother. Two years later, he matriculated at Queen's University at Kingston in Canada. Musk later transferred to the University of Pennsylvania, and received bachelor's degrees in economics and physics. He moved to California in 1995 to attend Stanford University, but dropped out after two days and, with his brother Kimbal, co-founded online city guide software company Zip2. The startup was acquired by Compaq for $307 million in 1999, and that same year Musk co-founded X.com, a direct bank. X.com merged with Confinity in 2000 to form PayPal. 
+    """
 
-    interests_chain = get_interests_chain()
-    interests = interests_chain.run(information=linkedin_data, twitter_posts=tweets)
-    interests = topics_of_interest_parser.parse(interests)
+    summary_template = """
+    given the information {information} about a person I want you to create:
+    1. A short summary
+    2. two interesting facts about them
+    """
 
-    ice_breaker_chain = get_ice_breaker_chain()
-    ice_breakers = ice_breaker_chain.run(
-        information=linkedin_data, twitter_posts=tweets
-    )
-    ice_breakers = ice_breaker_parser.parse(ice_breakers)
+    summary_prompt_template = PromptTemplate(input_variables=["information"], template=summary_template)
 
-    return (
-        summary_and_facts,
-        interests,
-        ice_breakers,
-        linkedin_data.get("profile_pic_url"),
-    )
+    # temperature will decide how creative the language model will be.
+    # 0 means it won't be creative
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
 
+    chain= LLMChain(llm=llm, prompt=summary_prompt_template)
+    res = chain.invoke(input={"information": information})
 
-if __name__ == "__main__":
-    pass
+    print(res)
+    
