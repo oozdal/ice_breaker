6c43d0b6cf44e6596c043eb7491b11d2a33747d0
Ozer, Ozdal <ozerozdal@gmail.com>
2024-03-06 17:15:44 -0500

    custom agent implementation

--- ice_breaker.py
+++ ice_breaker.py
@@ -2,7 +2,9 @@ from dotenv import load_dotenv
 from langchain.chains import LLMChain
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 from third_parties.linkedin import scrape_linkedin_profile
+
 
 if __name__ == "__main__":
     load_dotenv()
@@ -14,22 +16,25 @@ if __name__ == "__main__":
     A member of the wealthy South African Musk family, Elon was born in Pretoria and briefly attended the University of Pretoria before immigrating to Canada at age 18, acquiring citizenship through his Canadian-born mother. Two years later, he matriculated at Queen's University at Kingston in Canada. Musk later transferred to the University of Pennsylvania, and received bachelor's degrees in economics and physics. He moved to California in 1995 to attend Stanford University, but dropped out after two days and, with his brother Kimbal, co-founded online city guide software company Zip2. The startup was acquired by Compaq for $307 million in 1999, and that same year Musk co-founded X.com, a direct bank. X.com merged with Confinity in 2000 to form PayPal. 
     """
 
+    linkedin_profile_url = linkedin_lookup_agent(name="Ozer Ozdal")
+
     summary_template = """
     given the information {information} about a person I want you to create:
     1. A short summary
     2. two interesting facts about them
     """
 
-    summary_prompt_template = PromptTemplate(input_variables=["information"], template=summary_template)
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
 
     # temperature will decide how creative the language model will be.
     # 0 means it won't be creative
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
 
-    chain= LLMChain(llm=llm, prompt=summary_prompt_template)
-    
+    chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+
     linkedin_data = scrape_linkedin_profile(
-    linkedin_profile_url="https://www.linkedin.com/in/harrison-chase-961287118/"
-    )
-    
-    print(chain.run(input={"information": linkedin_data}))
+        linkedin_profile_url=linkedin_profile_url)
+
+    print(chain.invoke(input={"information": linkedin_data}))
